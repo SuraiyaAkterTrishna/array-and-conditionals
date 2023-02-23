246c6162e12ca399398c65259ead24e91342e60f
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-23 20:56:17 +0600

    2 ways to show array element in console.log()

---
+++ index.js
@@ -0,0 +1,6 @@
+// 2 ways to show array element in console.log()
+var friendsAge = [21, 22, 31, 41, 51];
+console.log(friendsAge);
+for(var i= 0; i<friendsAge.length; i++ ) {
+    console.log(friendsAge[i]);
+}
