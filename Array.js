0372d8482e697c882df25ea257a17e06c87d5a4c
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-24 08:32:13 +0600

    array push

--- Array.js
+++ Array.js
@@ -2,7 +2,11 @@
 var myFriends = [ 'Samiya', 'Lamiya', 'Maliha', 'Alija',  'Alia'];
 
 myFriends[2] = "Daliya";
-console.log(myFriends);
+// console.log(myFriends);
 
 var positionOfAlija = myFriends.indexOf("Alija");
-console.log(positionOfAlija);
+// console.log(positionOfAlija);
+
+// push()
+myFriends.push("Katrina");
+console.log(myFriends);
