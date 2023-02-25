6f3a5e6d0345f98931beb4de316c4fe52b24b1be
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-25 18:30:01 +0600

    practice problem 2 done

---
+++ practise2.js
@@ -0,0 +1,23 @@
+var Rony = 85
+var Tom = 66;
+var Jane = 95;
+var Peter = 56;
+var John = 40;
+
+if(John >= 80) {
+    console.log("A grade");
+}
+else if(John >= 60) {
+    console.log("B grade");
+}
+else if(John >=50){
+    console.log("C grade");
+}
+else if(John >=40){
+    console.log("D grade");
+}
+else {
+    console.log("F grade")
+}
+
+
