74cfe3a9fb36c9abf851dfd96586b5ed4fd26f96
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-25 18:41:01 +0600

    practise problem 4 done

---
+++ practise4.js
@@ -0,0 +1,9 @@
+var triangleSideA = 10;
+var triangleSideB = 11;
+var triangleSideC = 19; 
+if(triangleSideA == triangleSideB || triangleSideB == triangleSideC || triangleSideA == triangleSideC){
+    console.log("The triangle is Isosceles.")
+}
+else {
+    console.log("The triangle is not a Isosceles.")
+}
