d53572adc2afd172c3b0777413235a9ea8d2194d
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-25 18:35:01 +0600

    practise problem 3 done

---
+++ practise3.js
@@ -0,0 +1,15 @@
+var a = 13;
+var b = 79;
+var c = 45;
+
+if (a > b){
+    if(a > c){
+        console.log("Largest number is a: ", a);
+    }
+}
+else if (b > c){
+    console.log("Largest number is b: ", b);
+}
+else {
+    console.log("Largest number is c: ", c);
+}
