cb6d66c34bec1f0160f8821ac1d16e2b7e0fea8d
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-25 15:54:40 +0600

    equal ==, not !=

--- comparison.js
+++ comparison.js
@@ -3,3 +3,11 @@ console.log(8 > 6);
 console.log(10 < 12);
 console.log(15 < 10);
 console.log(25 > 100);
+
+// equal ==, not !=
+console.log(5 == 6);
+console.log(5 == 5);
+console.log(5 != 5);
+console.log(5 != 6);
+
+// ===, !==
