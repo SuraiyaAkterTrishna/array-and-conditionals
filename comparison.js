0faaab7f5ab0161dfebeaf4a1936696c2d3ff808
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-25 15:58:39 +0600

    ===, !==

--- comparison.js
+++ comparison.js
@@ -1,13 +1,23 @@
 // greater than > , less than < 
-console.log(8 > 6);
+/* console.log(8 > 6);
 console.log(10 < 12);
 console.log(15 < 10);
-console.log(25 > 100);
+console.log(25 > 100); */
 
 // equal ==, not !=
-console.log(5 == 6);
+/* console.log(5 == 6);
 console.log(5 == 5);
 console.log(5 != 5);
-console.log(5 != 6);
+console.log(5 != 6); */
 
 // ===, !==
+var num1 = 25;
+var num2 = 36;
+var num3 = "25";
+var num4 = "36";
+
+console.log(num1 == num3);
+console.log(num1 === num3);
+
+console.log(num2 != num4);
+console.log(num2 !== num4);
