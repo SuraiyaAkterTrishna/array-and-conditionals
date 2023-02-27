df59b7ff0aa00ab65c387aeb61474746cbf92923
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-27 22:23:24 +0600

    nop

--- substring.js
+++ substring.js
@@ -1,1 +1,3 @@
-var message = "I am very happy today because there is no mosquito in my home."
+var message = "I am very happy today because there is no mosquito in my home.";
+message.substring(5, 8);
+console.log(message);
